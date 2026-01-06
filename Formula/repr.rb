dbe46bcad15239e53ad66e29ffe3e9955afbd1bc
Mendrika Ramarlina <m.ramarlina@gmail.com>
2026-01-06 05:51:57 -0800

    Update formula to v0.2.5 with correct URLs and SHA256

--- Formula/repr.rb
+++ Formula/repr.rb
@@ -5,23 +5,13 @@ class Repr < Formula
   license "MIT"
 
   on_macos do
-    if Hardware::CPU.arm?
-      url "https://github.com/repr-app/cli/releases/download/v#{version}/repr-macos-arm64.tar.gz"
-      sha256 "PLACEHOLDER_MACOS_ARM64_SHA256"
-    else
-      url "https://github.com/repr-app/cli/releases/download/v#{version}/repr-macos-x86_64.tar.gz"
-      sha256 "PLACEHOLDER_MACOS_X86_64_SHA256"
-    end
+    url "https://github.com/repr-app/cli/releases/download/v#{version}/repr-macos.tar.gz"
+    sha256 "04eff1ac614b7f364e67dab2b8419166edfab1323b0476780988ae9d52196e7d"
   end
 
   on_linux do
-    if Hardware::CPU.arm?
-      url "https://github.com/repr-app/cli/releases/download/v#{version}/repr-linux-arm64.tar.gz"
-      sha256 "PLACEHOLDER_LINUX_ARM64_SHA256"
-    else
-      url "https://github.com/repr-app/cli/releases/download/v#{version}/repr-linux-x86_64.tar.gz"
-      sha256 "PLACEHOLDER_LINUX_X86_64_SHA256"
-    end
+    url "https://github.com/repr-app/cli/releases/download/v#{version}/repr-linux.tar.gz"
+    sha256 "b14fbba56ee90dbec1e6fa3875f9f42a0871ecdfb62eddd8f56a36e9ebf2d13b"
   end
 
   def install
